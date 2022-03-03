101b869fdff1ba781d3d65996d134c04bc907deb
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-03-03 18:07:26 +0530

    Create sort012.java

---
+++ leetcode/sort012.java
@@ -0,0 +1,32 @@
+package leetcode;
+
+class Solution {
+    public void swap(int[] arr,int i,int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public void sortColors(int[] arr){
+    int n = arr.length;    
+    int i = 0;
+    int j = 0;
+    int k = n-1;
+    while(j<=k){
+        int val = arr[j];
+        if(val==0){
+            swap(arr,i,j);
+            i++;
+            j++;
+        }
+        else if(val==1){
+            j++;
+        }
+        else{
+            swap(arr,j,k);
+            //j++;
+            k--;
+        }
+    }
+     
+  }
+}
