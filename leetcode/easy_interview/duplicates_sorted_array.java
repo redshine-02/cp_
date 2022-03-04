a54d7454d0115543d5e00ef3662a581fe024470d
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-03-04 08:57:16 +0530

    Day 1 Q 1

---
+++ leetcode/easy_interview/duplicates_sorted_array.java
@@ -0,0 +1,23 @@
+package leetcode.easy_interview;
+
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        int n=nums.length;
+        if(n<=1){
+            return n;
+        }
+        int count =1;
+        int i=1;
+        int j=1;
+        while(j<n){
+            if(nums[j]!=nums[j-1]){
+                nums[i]=nums[j];
+                i++;
+                count++;
+            }
+            j++;
+        }
+        
+       return count; 
+    }
+}
