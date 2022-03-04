be53b02a2dd1e273d11e97579c67706d0e38517e
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-03-04 10:04:56 +0530

    Day 1  Q 6

---
+++ leetcode/easy_interview/move_zero.java
@@ -0,0 +1,23 @@
+package leetcode.easy_interview;
+
+import java.util.*;
+class Solution {
+    public void swap(int[] arr,int i,int j){
+        int temp = arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
+    }
+    public void moveZeroes(int[] nums) {
+        int n = nums.length;
+        int i=0;
+        int j=0;
+        while(j<n){
+            if(nums[j]!=0){
+                swap(nums,i,j);
+                i++;
+            }
+            j++;
+        }
+        
+    }
+}
