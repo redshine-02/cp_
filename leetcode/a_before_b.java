b50574b60fb3c62acf59868cd29bc5839a6c2df0
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-03-06 16:36:49 +0530

    Update a_before_b.java

--- leetcode/a_before_b.java
+++ leetcode/a_before_b.java
@@ -3,15 +3,38 @@ import java.util.*;
 
 public class a_before_b {
     public boolean checkString(String s) {
-        int len = s.length();
-        for(int i=0;i<len;i++){
-            if(i+1<len){
-                if(s.charAt(i)=='b' && s.charAt(i+1)=='a'){
-                    return false;
-                }
+         int len = s.length();
+        // for(int i=0;i<len;i++){
+        //     if(i+1<len){
+        //         if(s.charAt(i)=='b' && s.charAt(i+1)=='a'){
+        //             return false;
+        //         }
+        //     }
+        // }
+        // return true;
+        int los=-1;
+        for(int i=len-1;i>=0;i--){
+            if(s.charAt(i)=='a'){
+                los=i;
+                break;
             }
         }
-        return true;
-        
+        int pos=-1;
+        for(int i=0;i<len;i++){
+            if(s.charAt(i)=='b'){
+                pos=i;
+                break;
+            }
+        }
+        if(pos>los){
+            return true;
+        }
+        else{
+            if(pos==-1){
+                return true;
+            }
+            else{
+            return false;
+            }        }
     }
 }
