2c62ed3eb2623f0ffbb97b6dd6d971b1c74ce4e1
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-06-18 16:57:09 +0530

    Update 983.java

--- leetcode/Dp/983.java
+++ leetcode/Dp/983.java
@@ -1,54 +1,33 @@
 package leetcode.Dp;
+import java.util.*;
 
+// class Solution {
 class Solution {
-    public int mincostTickets(int[] days, int[] costs) {
-        int n = days.length;
-        int m = costs.length;
-        int[][] dp = new int[m][n];
-        dp[0][0]=costs[0];
-        for(int i = 0 ; i<m ; i++){
-            for(int j = 0 ; j<n ; j++){
-                if(i == 0 && j>0){
-                    dp[i][j] = dp[i][j-1] + costs[i];
-                }
-                else if(i>0 && j==0){
-                    dp[i][j]=Math.min(dp[i-1][j],costs[i]);
+        public int[] dp = new int[366];
+        
+        public int mincostTickets(int[] days, int[] costs) {
+            HashSet<Integer> hs = new HashSet<>();
+            for(int day : days){hs.add(day);}
+            for(int i = 1 ; i<366 ; i++){
+                if(hs.contains(i)){
+                    int one = costs[0];
+                    int seven = costs[1];
+                    int thirty = costs[2];
+                    if(i-1>=0){
+                        one+=dp[i-1];
+                    }
+                    if(i-7>=0){
+                        seven+=dp[i-7];
+                    }
+                    if(i-30>=0){
+                        thirty+=dp[i-30];
+                    }
+                    dp[i] = Math.min(one , Math.min(seven,thirty));
                 }
                 else{
-                    if(i==1 && days[j]>=7){
-                        int d = days[j]/7;
-                        int r = days[j]%7;
-                        if(dp[i][j-1]>d*costs[i]){
-                            dp[i][j] = d*costs[i] + r*Math.min(costs[1],costs[0]);
-                        }
-                        else{
-                            dp[i][j] = dp[i][j-1] + Math.min(costs[1],costs[0]);
-                        }
-                        
-                    }
-                    else if(i==2 && days[j]>=30){
-                        int d = days[j]/30;
-                        int r = days[j]%30;
-                        if(dp[i][j-1]>d*costs[i]){
-                            dp[i][j] = d*costs[i] + r*Math.min(costs[1],Math.min(costs[0],costs[2]));
-                        }
-                        else{
-                            dp[i][j] = dp[i][j-1] + Math.min(costs[1],Math.min(costs[0],costs[2]));
-                        }
-                    }
-                    else{
-                        if(i>0){
-                        dp[i][j] = dp[i-1][j];}
-                    }
+                    dp[i] = dp[i-1];
                 }
             }
+            return dp[365];
         }
-        for(int i = 0 ; i<dp.length ; i++){
-            for(int j =0 ; j<dp[0].length ; j++){
-                System.out.print(dp[i][j]+" ");
-            }
-            System.out.println();
-        }
-        return dp[m-1][n-1];
     }
-}
