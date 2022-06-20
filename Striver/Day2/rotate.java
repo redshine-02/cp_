bb322665d8d047a3691aaddfe0e1d780835b7d3f
PALAK SINGH <80193531+Palaksingh02@users.noreply.github.com>
2022-06-20 16:20:34 +0530

    Create rotate.java

---
+++ Striver/Day2/rotate.java
@@ -0,0 +1,37 @@
+package Striver.Day2;
+
+public class rotate {
+    public static void transpose(int[][] a){
+        int n=a.length;
+        for(int i=0;i<n;i++){
+          for(int j=0;j<n;j++){
+            if(i<j){
+                int temp=a[i][j];
+                a[i][j]=a[j][i];
+                a[j][i]=temp;
+            }
+         }  
+        }
+    }
+    
+    public static void reverse(int[][] a){
+        int n=a.length;
+        for(int i=0;i<n;i++){
+            int lo=0;
+            int hi=n-1;
+            while(lo<hi){
+                int temp=a[i][lo];
+                a[i][lo]=a[i][hi];
+                a[i][hi]=temp;
+                lo++;
+                hi--;
+            }
+        }
+    }
+    
+    
+    public void rotate(int[][] matrix) {
+        transpose(matrix);
+        reverse(matrix);
+    }
+}
